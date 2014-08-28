1e0b0e623e83c605b1ca6a28c4a5712300cec957
pinebender <cheuk.timothy@gmail.com>
2014-08-28 12:51:03 -0500

    First Commit

---
+++ GeoQuiz/src/com/tim/geoquiz/QuizActivity.java
@@ -0,0 +1,209 @@
+package com.tim.geoquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class QuizActivity extends ActionBarActivity {
+
+	private Button mTrueButton;
+	private Button mFalseButton;
+	private Button mNextButton;
+	private Button mPrevButton;
+	private Button mCheatButton;
+	private TextView mQuestionTextView;
+	private static final String TAG = "QuizActivity";
+	private static final String KEY_INDEX = "index";
+	
+	private TrueFalse[] mQuestionBank = new TrueFalse[] {
+			new TrueFalse(R.string.question_oceans, true),
+			new TrueFalse(R.string.question_mideast, false),
+			new TrueFalse(R.string.question_africa, false),
+			new TrueFalse(R.string.question_americas, true),
+			new TrueFalse(R.string.question_asia, true),
+	};
+	
+	private int mCurrentIndex = 0;
+	
+	private boolean mIsCheater;
+	
+    private void updateQuestion() {
+    	//Log.d(TAG, "Updating question text for question #" + mCurrentIndex, new Exception());
+    	int question = mQuestionBank[mCurrentIndex].getQuestion();
+    	mQuestionTextView.setText(question);
+    }
+    
+    private void checkAnswer(boolean userPressedTrue) {
+    	boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+    	
+    	int messageResId = 0;
+    	
+    	if (mIsCheater) {
+    		messageResId = R.string.judgment_toast;
+    		
+    	} else {
+    	
+	    	if (userPressedTrue == answerIsTrue) {
+	    		messageResId = R.string.correct_toast;
+	    	} else {
+	    		messageResId = R.string.incorrect_toast;
+	    	}
+    	}
+    	Toast.makeText(this,  messageResId,  Toast.LENGTH_SHORT).show();
+    
+    	
+    }
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
+        setContentView(R.layout.activity_quiz);
+        
+        
+        
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+				
+			}
+		});
+
+        mPrevButton = (Button)findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mCurrentIndex = (mCurrentIndex - 1); // note that modulus in Java does keeps sign of remainder, so it won't wrap here
+				if (mCurrentIndex < 0) {
+					mCurrentIndex += mQuestionBank.length;
+					mIsCheater = false;
+				}	
+				updateQuestion();	
+			}
+		});
+        
+        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        		mIsCheater = false;
+        		updateQuestion();
+        	}
+        });
+        
+        if (savedInstanceState != null) {
+        	mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0); //returns KEY_INDEX or default value if no KEY_INDEX (0 in this case)
+        }
+        
+        
+        
+        mTrueButton = (Button)findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener(){
+        	@Override
+        	public void onClick(View v) {
+        		checkAnswer(true);
+        	}
+        });
+        
+        mFalseButton = (Button)findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				checkAnswer(false);
+	
+			}
+		});
+        
+        mCheatButton = (Button)findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+        	
+        	@Override
+        	public void onClick(View v) {
+        		//Start CheatActivity
+        		Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+        		boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        		i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+        		startActivityForResult(i, 0); //first argument is the intent; second argument is a user defined integer request code.
+        	}
+        });
+        
+        updateQuestion();
+    }
+	
+	@Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+    	super.onSaveInstanceState(savedInstanceState);
+    	Log.i(TAG, "onSaveInstanceState");
+    	savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+    
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");	
+	}
+	
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (data == null) {
+			return;
+		}
+		mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+	}
+	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
